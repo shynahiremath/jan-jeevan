e3188cd86fd3ec093d2959a9d009385333369559
shynahiremath <shynahiremath18@gmail.com>
2026-09-13 07:28:05 +0530

    Add shared Layout component

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Layout({ children, className = "" }) {
+  return (
+    <div className="min-h-screen flex flex-col bg-slate-50">
+      <Header />
+      <main className={`flex-1 ${className}`}>{children}</main>
+      <Footer />
+    </div>
+  );
+}
